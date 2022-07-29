b8ebe4dd2088c22c6a2644a93fe68eee60ee7c55
Jan Wiesemann <kontakt@janwiesemann.de>
2022-07-29 21:46:27 +0200

    Update can be executed on all nodes

--- updateEverything.sh
+++ updateEverything.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ADDITIONAL_SCRIPTS_DIRECTORY="./updateEverything.d/"
+ADDITIONAL_SCRIPTS_DIRECTORY=$(realpath "./updateEverything.d/")
+TMP_DIR="~/.pvescripts_tmp"
 
 #debugging helper
 if [[ -f "./debugHelper.sh" ]]; then
@@ -12,6 +13,9 @@ fi
 executeHost=false
 executeLXC=false
 executeCustom=false
+
+#if this is true, the preparing will not be executed. The script will actually be invoked
+isInvoke=false
 
 #Parse command line arguments
 for var in "$@"
@@ -26,89 +30,115 @@ do
         executeLXC=true
     elif [[ "$var" =~ "custom" ]]; then
         executeCustom=true
+    elif [[ "$var" =~ "currentNode" ]]; then
+        isInvoke=true
     fi
 done
 
 #if no arguement was set use 'all'
-if [[ $executeHost = false && $executeHost = false && $executeCustom = false ]]; then
+if [[ $executeHost = false && $executeLXC = false && $executeCustom = false ]]; then
     executeHost=true
     executeLXC=true
     executeCustom=true
 fi
 
-#Update Host OS
-if [[ $executeHost = true ]]; then
-    echo "==== Host ===="
-    apt update -y
-    apt upgrade -y
-
-    echo "Host Done!"
-    echo
-fi
-
 snapshotName="updateSnapshot_$(date +%Y_%m_%d_%k_%M)"
 export snapshotName
 
-#update containers and create snapshots if updates are avalible
-if [[ $executeHost = true ]]; then
-echo "Searching Containers...."
-    containers=$(pct list | tail -n +2 | cut -f1 -d' ')
+if [[ $isInvoke = false ]]; then
+    echo "Searching Nodes..."
+    nodes=$(pvecm nodes | tail -n +5 | cut -f21 -d' ')
     echo
-
-    for container in $containers
+    for node in $nodes
     do
-        status=`pct status $container`
-        if [ "$status" == "status: running" ]; then
-            echo "==== $container ===="
+        echo "====== $node ======"
 
-            echo "refreshing package feed..."
-            pct exec $container -- bash -c "apt update -y 2>/dev/null" > /dev/null
+        echo "preparing..."
+        ssh -t root@$node "mkdir -p $TMP_DIR" 2> /dev/null
+        scp "$(realpath "${BASH_SOURCE[0]}")" "root@$node:$TMP_DIR" > /dev/null #current file
 
-            numOfUpdatesAvalible=$(pct exec $container -- bash -c "apt list --upgradable 2>/dev/null | wc -l")
-            hasUpdateScript=$(pct exec $container -- bash -c "ls ~ | grep update.sh | wc -l")
+        scriptFile="$TMP_DIR/$(basename $0)"
+        echo "executing '$scriptFile' on $node..."
 
-            if [ $numOfUpdatesAvalible -gt 1 ] || [ $hasUpdateScript -gt 0 ]; then
-                    echo "found updates! creating snapshot..."
-                    pct snapshot $container $snapshotName > /dev/null
+        #invoke update script
+        ssh -t root@$node "bash $scriptFile currentNode $@"
 
-                    if [ $numOfUpdatesAvalible -gt 1 ]; then
-                            echo "installing $((numOfUpdatesAvalible - 1)) updates..."
-                            pct exec $container -- apt upgrade -y
-                    fi
+        echo "cleanup...";
+        ssh -t root@$node "rm -rf $TMP_DIR" 2> /dev/null
+        echo
+    done
 
-                    if [ $hasUpdateScript -gt 0 ]; then
-                            echo "executing update.sh script"
-                            pct exec $container -- bash ~/update.sh
-                    fi
-            fi
+    #executing custom scripts from $ADDITIONAL_SCRIPTS_DIRECTORY
+    if [[ $executeCustom = true ]]; then
+        if [[ -d $ADDITIONAL_SCRIPTS_DIRECTORY ]]; then
+            echo "Found directory $ADDITIONAL_SCRIPTS_DIRECTORY"
+            echo "running additional scripts..."
 
-            echo "$container Done!"
-            echo
+            currentDir=$PWD
+
+            for file in $ADDITIONAL_SCRIPTS_DIRECTORY*.sh; do
+                if [[ "$file" != "$ADDITIONAL_SCRIPTS_DIRECTORY*.sh" ]]; then
+                    cd "$currentDir" #reset workdir for every subscript
+                    echo "==== $file ===="
+
+                    bash $file
+
+                    echo "$file Done!"
+                    echo
+                fi
+            done
+
+            cd "$currentDir"
         fi
-    done;
-fi
+    fi
+else
+    #Update Host OS
+    if [[ $executeHost = true ]]; then
+        echo "==== Host ===="
+        apt update -y
+        apt upgrade -y
 
-#executing custom scripts
-if [[ $executeCustom = true ]]; then
-    if [[ -d $ADDITIONAL_SCRIPTS_DIRECTORY ]]; then
-        echo "Found directory $ADDITIONAL_SCRIPTS_DIRECTORY"
-        echo "running additional scripts..."
+        echo "Host Done!"
+        echo
+    fi
 
-        currentDir=$PWD
+    #update containers and create snapshots if updates are avalible
+    if [[ $executeLXC = true ]]; then
+        echo "Searching Containers...."
+        containers=$(pct list | tail -n +2 | cut -f1 -d' ')
+        echo
 
-        for file in $ADDITIONAL_SCRIPTS_DIRECTORY*.sh; do
-            if [[ "$file" != "$ADDITIONAL_SCRIPTS_DIRECTORY*.sh" ]]; then
-                cd "$currentDir" #reset workdir for every subscript
-                echo "==== $file ===="
+        for container in $containers
+        do
+            status=`pct status $container`
+            if [ "$status" == "status: running" ]; then
+                echo "==== $container ===="
 
-                bash $file
+                echo "refreshing package feed..."
+                pct exec $container -- bash -c "apt update -y 2>/dev/null" > /dev/null
 
-                echo "$file Done!"
+                numOfUpdatesAvalible=$(pct exec $container -- bash -c "apt list --upgradable 2>/dev/null | wc -l")
+                hasUpdateScript=$(pct exec $container -- bash -c "ls ~ | grep update.sh | wc -l")
+
+                if [ $numOfUpdatesAvalible -gt 1 ] || [ $hasUpdateScript -gt 0 ]; then
+                        echo "found updates! creating snapshot..."
+                        pct snapshot $container $snapshotName > /dev/null
+
+                        if [ $numOfUpdatesAvalible -gt 1 ]; then
+                                echo "installing $((numOfUpdatesAvalible - 1)) updates..."
+                                pct exec $container -- apt upgrade -y
+                        fi
+
+                        if [ $hasUpdateScript -gt 0 ]; then
+                                echo "executing update.sh script"
+                                pct exec $container -- bash ~/update.sh
+                        fi
+                fi
+
+                echo "$container Done!"
                 echo
             fi
-        done
-
-        cd "$currentDir"
+        done;
     fi
 fi
 
